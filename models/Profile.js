3dc78f0593ea2c43a4878e123e738203b3641c84
Mario <mjvinti@gmail.com>
2019-05-09 14:35:31 -0400

    add and delete profile education

--- models/Profile.js
+++ models/Profile.js
@@ -67,7 +67,7 @@ const ProfileSchema = new mongoose.Schema({
         type: String,
         required: true
       },
-      fieldsofstudy: {
+      fieldofstudy: {
         type: String,
         required: true
       },
