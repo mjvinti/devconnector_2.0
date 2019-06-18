29d41e431da6b2aaa9ec2e718f21379685570b24
Mario <mjvinti@gmail.com>
2019-06-17 23:08:38 -0400

    display profiles

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,9 +8,14 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
+        <Link to="/profiles">
+          Developers
+        </Link>
+      </li>
+      <li>
         <Link to="/dashboard">
-        <i className="fas fa-user" />{" "}  
-        <span className="hide-sm">Dashboard</span>
+          <i className="fas fa-user" />{" "}
+          <span className="hide-sm">Dashboard</span>
         </Link>
       </li>
       <li>
@@ -24,7 +29,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
 
   const guestLinks = (
     <ul>
-      <li><Link to="#!">Developers</Link></li>
+      <li><Link to="/profiles">Developers</Link></li>
       <li><Link to="/register">Register</Link></li>
       <li><Link to="/login">Login</Link></li>
     </ul>
